35c8e4bab22fa69c31d70aca11391a0731f95f67
Rodolfo Mariano de Souza <rodolfo.mariano@outlook.com>
2021-04-05 15:53:35 -0300

    Created SignIn page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
-import Head from 'next/head'
+import { Button, Flex, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
-export default function Home() {
+export default function SignIn() {
   return (
-    <h1>Dashgo</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        padding="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+
+          <Input name={'email'} label={'E-mail'} type={'email'} />
+
+          <Input name={'password'} label={'Senha'} type={'password'} />
+
+        </Stack>
+
+        <Button type="submit" marginTop="6" colorScheme="purple" size="lg">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
