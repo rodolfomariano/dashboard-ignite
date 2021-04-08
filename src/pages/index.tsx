d2a54c26d0fc486874023db60c4929d1c526c4cb
Rodolfo Mariano de Souza <rodolfo.mariano@outlook.com>
2021-04-08 13:12:45 -0300

    Finished UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
-import { Button, Flex, Stack } from '@chakra-ui/react'
+
+import { Button, Flex, Stack, useWhyDidYouUpdate } from '@chakra-ui/react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 import { Input } from '../components/Form/Input'
 
+type SignInFormData = {
+  email: string
+  password: string
+}
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória')
+})
+
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  })
+
+  const { errors } = formState
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    console.log(values)
+  }
+
   return (
     <Flex
       w="100vw"
@@ -17,16 +42,37 @@ export default function SignIn() {
         padding="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
 
-          <Input name={'email'} label={'E-mail'} type={'email'} />
+          <Input
+            name={'email'}
+            label={'E-mail'}
+            type={'email'}
+            error={errors.email}
+            {...register('email')}
+          />
 
-          <Input name={'password'} label={'Senha'} type={'password'} />
+          <Input
+            name={'password'}
+            label={'Senha'}
+            type={'password'}
+            error={errors.password}
+            {...register('password')}
+          />
 
         </Stack>
 
-        <Button type="submit" marginTop="6" colorScheme="purple" size="lg">Entrar</Button>
+        <Button
+          type="submit"
+          marginTop="6"
+          colorScheme="purple"
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
+          Entrar
+        </Button>
       </Flex>
     </Flex>
   )
