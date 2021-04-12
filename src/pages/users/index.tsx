9d9e9be1755507d763d49492d84cf8fca04834eb
Rodolfo Mariano de Souza <rodolfo.mariano@outlook.com>
2021-04-12 11:18:11 -0300

    Using ReactQueryDevtools

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,7 +12,22 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
 
-    return data
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    })
+
+    return users
+  }, {
+    staleTime: 1000 * 5, // 5 segundos
   })
 
   const isWideVersion = useBreakpointValue({
@@ -57,129 +72,61 @@ export default function UserList() {
           ) : (
             <>
               <Table colorScheme="whiteAlpha">
-              <Thead>
-                <Tr>
-                  <Th px={["4", "4", "6"]} color="gray.300" width="8" >
-                    <Checkbox colorScheme="purple" />
-                  </Th>
-                  <Th>Usuário</Th>
-                  { isWideVersion && <Th>Data de cadastro</Th> }
-                  <Th></Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                <Tr>
-                  <Td px={["4", "4", "6"]}>
-                    <Checkbox colorScheme="purple" />
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">User Name</Text>
-                      <Text fontSize="sm" color="gray.300">username@gmail.com</Text>
-                    </Box>
-                  </Td>
-                  { isWideVersion && <Td color="gray.200">04 de abril,2021</Td> }
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      mr="2"
-                      cursor="pointer"
-                      
-                    >
-                      <Icon as={RiPencilLine} />
-                    </Button>
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8" >
+                      <Checkbox colorScheme="purple" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    { isWideVersion && <Th>Data de cadastro</Th> }
+                    <Th></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  { data.map(user => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme="purple" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                          </Box>
+                        </Td>
+                        { isWideVersion && <Td color="gray.200">{user.createdAt}</Td> }
+                        <Td>
+                          <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="purple"
+                            mr="2"
+                            cursor="pointer"
+                            
+                          >
+                            <Icon as={RiPencilLine} />
+                          </Button>
 
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="red"
-                      cursor="pointer"
-                    >
-                      <Icon as={RiDeleteBin4Line} />
-                    </Button>
-                  </Td>
-                </Tr>
+                          <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="red"
+                            cursor="pointer"
+                          >
+                            <Icon as={RiDeleteBin4Line} />
+                          </Button>
+                        </Td>
+                      </Tr>
+                    )
+                  }) }
 
-                <Tr>
-                  <Td px={["4", "4", "6"]}>
-                    <Checkbox colorScheme="purple" />
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">User Name</Text>
-                      <Text fontSize="sm" color="gray.300">username@gmail.com</Text>
-                    </Box>
-                  </Td>
-                  { isWideVersion && <Td color="gray.200">04 de abril,2021</Td> }
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      mr="2"
-                      cursor="pointer"
-                      
-                    >
-                      <Icon as={RiPencilLine} />
-                    </Button>
+                </Tbody>
+              </Table>
 
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="red"
-                      cursor="pointer"
-                    >
-                      <Icon as={RiDeleteBin4Line} />
-                    </Button>
-                  </Td>
-                </Tr>
-
-                <Tr>
-                  <Td px={["4", "4", "6"]}>
-                    <Checkbox colorScheme="purple" />
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">User Name</Text>
-                      <Text fontSize="sm" color="gray.300">username@gmail.com</Text>
-                    </Box>
-                  </Td>
-                  { isWideVersion && <Td color="gray.200">04 de abril,2021</Td> }
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      mr="2"
-                      cursor="pointer"
-                      
-                    >
-                      <Icon as={RiPencilLine} />
-                    </Button>
-
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="red"
-                      cursor="pointer"
-                    >
-                      <Icon as={RiDeleteBin4Line} />
-                    </Button>
-                  </Td>
-                </Tr>
-                
-              </Tbody>
-            </Table>
-
-            <Pagination />
+              <Pagination />
             </>
           ) }
 
